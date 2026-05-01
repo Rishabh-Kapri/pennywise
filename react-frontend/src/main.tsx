30c432390e5b2f02f80d8523ae4cd7e6a2c5fd66
Rishabh Kapri <rishabhkapri@gmail.com>
2026-05-01 18:32:58 +0530

    chore(react-frontend): remove StrictMode for railway

--- react-frontend/src/main.tsx
+++ react-frontend/src/main.tsx
@@ -7,15 +7,15 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || '';
 
-console.log('GOOGLE_CLIENT_ID', GOOGLE_CLIENT_ID);
+const AppTree = (
+  <HeroUIProvider>
+    <ToastProvider placement="bottom-right" toastOffset={16} />
+    <App />
+  </HeroUIProvider>
+);
 createRoot(document.getElementById('root')!).render(
   <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-    <StrictMode>
-      <HeroUIProvider>
-        <ToastProvider placement="bottom-right" toastOffset={16} />
-        <App />
-      </HeroUIProvider>
-    </StrictMode>
-    ,
+    {import.meta.env.RAILWAY_ENVIRONMENT_NAME ? AppTree : <StrictMode>{AppTree}</StrictMode>}
+    <StrictMode></StrictMode>,
   </GoogleOAuthProvider>,
 );
